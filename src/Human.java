01ec57667ba58d28e29c335f0b51ebbca3c7a60a
DanilRose <totkto49@gmail.com>
2023-11-13 16:48:48 +0300

    Объединили сущности Human и Names. Теперь, у нас в сущности Human, имя задается сущностью Names.

--- src/Human.java
+++ src/Human.java
@@ -1,16 +1,24 @@
 public class Human {
-    String name;
+    Names name;
     int height;
 
-    public Human (String name, int height){
-        this.name = name;
+    public Human (Names name,int height){
+        this.name =  name;
         this.height = height;
     }
     public String toString(){
-        return "Человек с именем:" + " " + name + " " + "и ростом:" + " " + height ;
+        return "Человек с именем:" + " " + name + "и ростом:" + " " + height ;
     }
+
     public static void test1_2(){
-        Human human1 = new Human("Клеопатра", 152);
+        Names name1 = new Names("Клеопатра", "", "");
+        Names name2 = new Names("Александр", "Сергеевич", "Пушкин");
+        Names name3 = new Names("Владимир", "Маяковский", " ");
+        Human human2 = new Human(name2, 167);
+        Human human1 = new Human(name1, 152);
+        Human human3 = new Human(name3, 189);
         System.out.println(human1);
+        System.out.println(human2);
+        System.out.println(human3);
     }
 }
